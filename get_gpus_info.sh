08a7f55fa9ba4779a9f631839671150322ae1cd1
PhoenixEmik <phoenix0919mik@gmail.com>
2025-04-04 00:38:28 +0800

    Refactor GPU data handling in get_gpus_info.sh for improved clarity and consistency

--- get_gpus_info.sh
+++ get_gpus_info.sh
@@ -8,13 +8,13 @@ if [ -z "$result" ]; then
   exit 0
 fi
 
-declare -a gpu_data
+declare -a gpu_entry_array
 
 while IFS= read -r line; do
   index=$(echo "$line" | awk -F':| ' '{print $2}')
-  gpuuuid=$(echo "$line" | grep -oP '(?<=UUID: GPU-)[^)]+')
+  gpuuuid=$(echo "$line" | grep -oP '(?<=UUID: )GPU-[^)]+')
 
-  gpu_data+=("{\"{#GPUINDEX}\":\"$index\", \"{#GPUUUID}\":\"GPU-$gpuuuid\"}")
+  gpu_entry_array+=("{\"{#GPUINDEX}\":\"$index\", \"{#GPUUUID}\":\"$gpuuuid\"}")
 done < <(printf '%s\n' "$result")
 
-echo -e "{\n\"data\":[\n$(IFS=,; echo "${gpu_data[*]}")\n]\n}"
+echo -e "{\n\"data\":[\n$(IFS=,; echo "${gpu_entry_array[*]}")\n]\n}"
